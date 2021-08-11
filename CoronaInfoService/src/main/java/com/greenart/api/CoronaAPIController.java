9a2bfc823a1f4423cd424245741d6fb87da92d02
hyeri1996 <bis03037@naver.com>
2021-08-11 18:08:09 +0900

    코로나 시도 및 연령추가2

--- CoronaInfoService/src/main/java/com/greenart/api/CoronaAPIController.java
+++ CoronaInfoService/src/main/java/com/greenart/api/CoronaAPIController.java
@@ -25,7 +25,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 
 @RestController
 public class CoronaAPIController {
